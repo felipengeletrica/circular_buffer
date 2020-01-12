af1c5e3f74d0fee38e8cda0881de0ba9e273279e
Felipe <felipeng.eletrica@gmail.com>
2020-01-12 02:01:15 -0300

    ADD c++ portability

--- ring_buffer.h
+++ ring_buffer.h
@@ -4,6 +4,10 @@
 /* INCLUDES */
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* STRUCTS */
 typedef struct {
     uint8_t * const buffer;
@@ -15,4 +19,8 @@ typedef struct {
 /* PROTOTYPES */
 int circ_buf_push(circ_buf_st *c, uint8_t data);
 int circ_buf_pop(circ_buf_st *c, uint8_t *data);
+
+#ifdef __cplusplus
+}
+#endif
 #endif /* RING_BUFFER_H */
