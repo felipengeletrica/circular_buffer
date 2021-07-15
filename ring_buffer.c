8d9fb712f89e3a44339d73587a7d106aff2d65d3
Felipe Vargas <felipeng.eletrica@gmail.com>
2021-07-15 18:03:26 -0300

    Testing generic buffer

---
+++ ring_buffer.c
@@ -0,0 +1,114 @@
+#include "ring_buffer.h"
+
+/**
+ * @brief Init Ring Buffer
+ * 
+ * @param buffer 
+ * @param size 
+ * @return ring_handle_buf_t 
+ */
+ring_handle_buf_t ring_buf_init(void* buffer, size_t size)
+{
+    /* Allocation */
+    ring_handle_buf_t ring_buf = malloc(sizeof(ring_buf_st));
+
+	ring_buf->buffer = buffer;
+	ring_buf->buffer_size = size;
+	
+	return ring_buf;
+}
+
+/**
+ * @brief Push data
+ * 
+ * @param st_ring_buf 
+ * @param data 
+ * @return int 
+ */
+int ring_buf_push(ring_handle_buf_t st_ring_buf, void * data)
+{
+    int next = 0;
+
+    next = st_ring_buf->head + 1; 
+    if (next > st_ring_buf->buffer_size)
+        next = 0;
+
+    if (next == st_ring_buf->tail)
+        return -1;
+
+    st_ring_buf->buffer[st_ring_buf->head] = &data;
+    st_ring_buf->head = next;
+    return 0;
+}
+
+/**
+ * @brief Pop data 
+ * 
+ * @param st_ring_buf 
+ * @param data 
+ * @return int 
+ */
+int ring_buf_pop(ring_handle_buf_t st_ring_buf, void *data)
+{
+    int next = 0;
+
+    if (st_ring_buf->head == st_ring_buf->tail)
+        return -1;
+
+    next = st_ring_buf->tail + 1;
+    if(next > st_ring_buf->buffer_size)
+        next = 0;
+
+    *data = st_ring_buf->buffer[st_ring_buf->tail];
+    st_ring_buf->tail = next;
+
+    return 0;
+}
+
+/**
+ * @brief flush buffer
+ * 
+ * @param st_ring_buf 
+ * @return int 
+ */
+int ring_buf_flush(ring_handle_buf_t st_ring_buf)
+{
+    st_ring_buf->tail = 0;
+    st_ring_buf->head = 0;
+    return 0;
+}
+
+/**
+ * @brief Free Buffer 
+ * 
+ * @param st_ring_buf 
+ * @return int 
+ */
+int ring_buf_free(ring_handle_buf_t st_ring_buf)
+{
+    /* Free memory */
+    free(st_ring_buf);
+    return 0;
+}
+
+/**
+ * @brief Read buffer size
+ * 
+ * @param st_ring_buf 
+ * @return size_t 
+ */
+size_t ring_buf_size(ring_handle_buf_t st_ring_buf)
+{
+    size_t size = 0;
+
+    if(st_ring_buf->head >= st_ring_buf->tail)
+    {
+        size = st_ring_buf->head >= st_ring_buf->tail;
+    }
+    else
+    {
+        size = (st_ring_buf->buffer_size + st_ring_buf->head) - st_ring_buf->tail;
+    }
+    
+    return size;
+}
---
+++ ring_buffer.h
@@ -0,0 +1,40 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+/* INCLUDES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* DEFINES */
+/* Flexible buffer data type */
+//typedef uint32_t type_data;
+
+/* STRUCTS */
+typedef struct {
+    void * buffer;
+    size_t head;
+    size_t tail;
+    size_t buffer_size;
+} ring_buf_st;
+
+/* VARIABLES */
+typedef ring_buf_st* ring_handle_buf_t;
+
+/* PROTOTYPES */
+ring_handle_buf_t ring_buf_init(void* buffer, size_t size);
+int ring_buf_push(ring_handle_buf_t st_ring_buf, void *data);
+int ring_buf_pop(ring_handle_buf_t st_ring_buf, void *data);
+int ring_buf_flush(ring_handle_buf_t st_ring_buf);
+int ring_buf_free(ring_handle_buf_t st_ring_buf);
+size_t ring_buf_size(ring_handle_buf_t st_ring_buf);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* RING_BUFFER_H */
